e9a9088d2124d6c3b4074f61b1f7ff389c4e5fdd
QuangHuydev <buiquanghuy0029a@gmail.com>
2023-04-21 21:24:11 +0700

    build default layout

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ import Following from '~/pages/Following';
 import Home from '~/pages/Home';
 import Profile from '~/pages/Profile';
 import Upload from '~/pages/Upload';
+import { HeaderOnly } from '~/components/Layout';
 
 const publicRoutes = [
     {

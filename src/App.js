b1b5f1c3cc7072909a27468dac424a8468c23958
QuangHuydev <buiquanghuy0029a@gmail.com>
2023-04-21 10:57:58 +0700

    config router, layout for tiktok

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
+import { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { publicRoutes } from '~/routes';
+import { DefaultLayout } from './components/Layout';
 function App() {
+    console.log(publicRoutes);
     return (
-        <div className="App">
-            <h1>React app</h1>
-        </div>
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        let Layout = DefaultLayout;
+                        if (route.layout) {
+                            Layout = route.layout;
+                        } else if (route.layout === null) {
+                            Layout = Fragment;
+                        }
+                        const Page = route.component;
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </div>
+        </Router>
     );
 }
 

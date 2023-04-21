bb7b59c5a78097e0f450d0e6553698e35f6989d6
QuangHuydev <buiquanghuy0029a@gmail.com>
2023-04-21 10:56:09 +0700

    config router for tiktok, setup layout

---
+++ src/routes/index.js
@@ -0,0 +1,27 @@
+import Following from '~/pages/Following';
+import Home from '~/pages/Home';
+import Profile from '~/pages/Profile';
+import Upload from '~/pages/Upload';
+
+const publicRoutes = [
+    {
+        path: '/',
+        component: Home,
+    },
+    {
+        path: '/following',
+        component: Following,
+    },
+    {
+        path: '/profile',
+        component: Profile,
+    },
+    {
+        path: '/upload',
+        component: Upload,
+        layout: HeaderOnly,
+    },
+];
+// private must login then use
+const privateRoutes = [];
+export { publicRoutes, privateRoutes };
